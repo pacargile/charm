2d6da320fbc1124e757fc1e6ba9dd573ac85ebef
Phillip Cargile <pcargile@cfa.harvard.edu>
2018-06-21 09:48:46 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import re
+
+try:
+    from setuptools import setup
+    setup
+except ImportError:
+    from distutils.core import setup
+    setup
+
+setup(
+    name="cham",
+    version='0.1',
+    author="Phill Cargile",
+    author_email="pcargile@cfa.harvard.edu",
+    packages=["cham"],
+    url="",
+    #license="LICENSE",
+    description="Cluster Heirarchical Astrometric Modeling",
+    long_description=open("README.md").read() + "\n\n",
+    #install_requires=["numpy", "scipy >= 0.9"],
+)
