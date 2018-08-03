5a5aad60175f556a536d92d4d79733bcf2b312e4
Phillip Cargile <pcargile@cfa.harvard.edu>
2018-08-03 15:53:18 -0400

    name change, cleaned up gitignore

--- build/lib/cham/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from __future__ import (division, print_function)
-from six.moves import range
-
-from . import model
-from . import fitting
-from . import priors
